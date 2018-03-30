0ef360809fca0d4b8a86a3afb347e062852111fc
Jud White <github@judsonwhite.com>
2018-03-30 13:31:25 -0500

    add ResourceDictionaries

--- YoloTrain/App.xaml.cs
+++ YoloTrain/App.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Windows;
+﻿using System;
+using System.Windows;
 using YoloTrain.Mvvm;
 using YoloTrain.Views;
 
@@ -9,9 +10,18 @@ namespace YoloTrain
     /// </summary>
     public partial class App : Application
     {
+        public App()
+        {
+            // Note: Known bug with App.xaml having no StartupUri and the theme speicified in XAML's Application.Resources
+            Resources = (ResourceDictionary)LoadComponent(new Uri("/Themes/Default/Theme.xaml", UriKind.RelativeOrAbsolute));
+        }
+
         protected override void OnStartup(StartupEventArgs e)
         {
-            IoC.Resolve<MainWindow>().Show();
+            base.OnStartup(e);
+
+            var mainWindow = IoC.Resolve<MainWindow>();
+            mainWindow.Show();
         }
     }
 }
