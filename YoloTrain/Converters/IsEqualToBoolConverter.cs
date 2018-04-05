7895807a19f5f3f32c221d93bd3c11a4ef644f9b
Jud White <github@judsonwhite.com>
2018-04-05 18:13:42 -0500

    add converters

---
+++ YoloTrain/Converters/IsEqualToBoolConverter.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace YoloTrain.Converters
+{
+    public class IsEqualToBoolConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var result = $"{value}" == $"{parameter}";
+            return result;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ YoloTrain/Converters/IsEqualToVisibilityConverter.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+
+namespace YoloTrain.Converters
+{
+    public class IsEqualToVisibilityConverter : IValueConverter
+    {
+        private static readonly IsEqualToBoolConverter _isEqualToBoolConverter = new IsEqualToBoolConverter();
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return (bool)_isEqualToBoolConverter.Convert(value, targetType, parameter, culture) ? Visibility.Visible : Visibility.Collapsed;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
