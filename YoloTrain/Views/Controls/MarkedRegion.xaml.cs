78330fad6d105cb736fae86b2508669e3b532166
Jud White <github@judsonwhite.com>
2018-04-07 04:01:46 -0500

    MarkRegion user control

---
+++ YoloTrain/Views/Controls/MarkedRegion.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System.Windows;
+using YoloTrain.Mvvm;
+
+namespace YoloTrain.Views.Controls
+{
+    /// <summary>
+    /// Interaction logic for MarkedRegion.xaml
+    /// </summary>
+    public partial class MarkedRegion : UserControlView
+    {
+        public static readonly DependencyProperty TextProperty =
+            DependencyProperty.Register("Text", typeof(string), typeof(MarkedRegion), new PropertyMetadata());
+
+        public static readonly DependencyProperty RegionIndexProperty =
+            DependencyProperty.Register("RegionIndex", typeof(int), typeof(MarkedRegion), new PropertyMetadata());
+
+        public static readonly DependencyProperty IsSelectedProperty =
+            DependencyProperty.Register("IsSelected", typeof(bool), typeof(MarkedRegion), new PropertyMetadata());
+
+        public MarkedRegion()
+        {
+            InitializeComponent();
+        }
+
+        public string Text
+        {
+            get => (string)GetValue(TextProperty);
+            set => SetValue(TextProperty, value);
+        }
+
+        public int RegionIndex
+        {
+            get => (int)GetValue(RegionIndexProperty);
+            set => SetValue(RegionIndexProperty, value);
+        }
+
+        public bool IsSelected
+        {
+            get => (bool)GetValue(IsSelectedProperty);
+            set => SetValue(IsSelectedProperty, value);
+        }
+
+    }
+}
